b85255d51585b2006938a078e1d32e23763e14e0
laurarodriguesfs <contato@laurarodrigues.com.br>
2025-05-29 17:21:55 -0300

    Criação do carossel e aplicação das fontes

--- js/components/materialize-init.js
+++ js/components/materialize-init.js
@@ -1,3 +1,23 @@
 export function reinicializarMaterialize() {
-    M.Parallax.init(document.querySelectorAll('.parallax'));
-  }  
+  // Inicializa o parallax
+  const parallax = document.querySelectorAll('.parallax');
+  M.Parallax.init(parallax);
+
+  // Inicializa o carrossel
+  const carousels = document.querySelectorAll('.carousel');
+  M.Carousel.init(carousels, {
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Setas customizadas
+  const instance = M.Carousel.getInstance(carousels[0]);
+
+  document.querySelector('.carousel-prev').addEventListener('click', () => {
+    instance.prev();
+  });
+
+  document.querySelector('.carousel-next').addEventListener('click', () => {
+    instance.next();
+  });
+}
--- js/components/pagehome.js
+++ js/components/pagehome.js
@@ -4,101 +4,49 @@ export default function home() {
   const main = document.querySelector("#main");
 
   main.innerHTML = `
-    <div id="index-banner" class="parallax-container">
-      <div class="section no-pad-bot">
-        <div class="container">
-          <br><br>
-          <h1 class="header center teal-text text-lighten-2">Parallax Template</h1>
-          <div class="row center">
-            <h5 class="header col s12 light">
-              A modern responsive front-end framework based on Material Design
-            </h5>
-          </div>
-          <div class="row center">
-            <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">
-              Get Started
-            </a>
-          </div>
-          <br><br>
-        </div>
-      </div>
-      <div class="parallax"><img src="background1.jpg" alt="Unsplashed background img 1"></div>
-    </div>
-
     <div class="container">
-      <div class="section">
-        <div class="row">
-          <div class="col s12 m4">
-            <div class="icon-block">
-              <h2 class="center brown-text"><i class="material-icons">flash_on</i></h2>
-              <h5 class="center">Speeds up development</h5>
-              <p class="light">
-                We did most of the heavy lifting for you to provide a default stylings that incorporate our custom components. Additionally, we refined animations and transitions to provide a smoother experience for developers.
-              </p>
-            </div>
-          </div>
+      <h3>Nossos projetos</h3>
 
-          <div class="col s12 m4">
-            <div class="icon-block">
-              <h2 class="center brown-text"><i class="material-icons">group</i></h2>
-              <h5 class="center">User Experience Focused</h5>
-              <p class="light">
-                By utilizing elements and principles of Material Design, we were able to create a framework that incorporates components and animations that provide more feedback to users. Additionally, a single underlying responsive system across all platforms allow for a more unified user experience.
-              </p>
-            </div>
+    <div class="carousel-container">
+      <div class="carousel">
+        <div class="carousel-item">
+          <img src="images/slide1.jpg?text=Slide+1" alt="Slide 1">
+          <div class="carousel-caption-below">
+            <h5>Título do Slide 1</h5>
+            <p>Descrição do Slide 1 que vai aparecer aqui.</p>
           </div>
+        </div>
 
-          <div class="col s12 m4">
-            <div class="icon-block">
-              <h2 class="center brown-text"><i class="material-icons">settings</i></h2>
-              <h5 class="center">Easy to work with</h5>
-              <p class="light">
-                We have provided detailed documentation as well as specific code examples to help new users get started. We are also always open to feedback and can answer any questions a user may have about Materialize.
-              </p>
-            </div>
+        <div class="carousel-item">
+          <img src="images/slide2.jpg?text=Slide+2" alt="Slide 2">
+          <div class="carousel-caption-below">
+            <h5>Título do Slide 2</h5>
+            <p>Descrição do Slide 2 que vai aparecer aqui.</p>
+          </div>
+        </div>
+
+        <div class="carousel-item">
+          <img src="images/slide3.jpg?text=Slide+3" alt="Slide 3">
+          <div class="carousel-caption-below">
+            <h5>Título do Slide 3</h5>
+            <p>Descrição do Slide 3 que vai aparecer aqui.</p>
+          </div>
+        </div>
+
+        <div class="carousel-item">
+          <img src="images/slide4.jpg?text=Slide+4" alt="Slide 4">
+          <div class="carousel-caption-below">
+            <h5>Título do Slide 4</h5>
+            <p>Descrição do Slide 4 que vai aparecer aqui.</p>
           </div>
         </div>
       </div>
     </div>
 
-    <div class="parallax-container valign-wrapper">
-      <div class="section no-pad-bot">
-        <div class="container">
-          <div class="row center">
-            <h5 class="header col s12 light">
-              A modern responsive front-end framework based on Material Design
-            </h5>
-          </div>
-        </div>
-      </div>
-      <div class="parallax"><img src="background2.jpg" alt="Unsplashed background img 2"></div>
-    </div>
 
-    <div class="container">
-      <div class="section">
-        <div class="row">
-          <div class="col s12 center">
-            <h3><i class="material-icons">send</i></h3>
-            <h4>Contact Us</h4>
-            <p class="left-align light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque id nunc nec volutpat. Etiam pellentesque tristique arcu, non consequat magna fermentum ac. Cras ut ultricies eros. Maecenas eros justo, ullamcorper a sapien id, viverra ultrices eros. Morbi sem neque, posuere et pretium eget, bibendum sollicitudin lacus. Aliquam eleifend sollicitudin diam, eu mattis nisl maximus sed. Nulla imperdiet semper molestie. Morbi massa odio, condimentum sed ipsum ac, gravida ultrices erat. Nullam eget dignissim mauris, non tristique erat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="parallax-container valign-wrapper">
-      <div class="section no-pad-bot">
-        <div class="container">
-          <div class="row center">
-            <h5 class="header col s12 light">
-              A modern responsive front-end framework based on Material Design
-            </h5>
-          </div>
-        </div>
-      </div>
-      <div class="parallax"><img src="background3.jpg" alt="Unsplashed background img 3"></div>
+    <!-- Setas -->
+    <img src="images/prev.svg" class="carousel-prev" style="position: absolute; top: 50%; left: 10px; cursor: pointer; width: 40px; height: 40px; transform: translateY(-50%);">
+    <img src="images/next.svg" class="carousel-next" style="position: absolute; top: 50%; right: 10px; cursor: pointer; width: 40px; height: 40px; transform: translateY(-50%);">
     </div>
   `;
 
