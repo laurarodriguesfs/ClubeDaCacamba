20c1fe9955eea7924545dfdf883065634abfc286
laurarodriguesfs <contato@laurarodrigues.com.br>
2025-05-29 22:09:54 -0300

    Inclusão das demais sessões e rodapé

--- js/components/footer.js
+++ js/components/footer.js
@@ -2,42 +2,28 @@ export default function footer() {
     const footer = document.querySelector("#footer");
   
     footer.innerHTML = ` 
-      <footer class="page-footer teal">
-        <div class="container">
-          <div class="row">
-            <div class="col l6 s12">
-              <h5 class="white-text">Company Bio</h5>
-              <p class="grey-text text-lighten-4">
-                We are a team of college students working on this project like it's our full time job.
-                Any amount would help support and continue development on this project and is greatly appreciated.
-              </p>
-            </div>
-            <div class="col l3 s12">
-              <h5 class="white-text">Settings</h5>
-              <ul>
-                <li><a class="white-text" href="#!">Link 1</a></li>
-                <li><a class="white-text" href="#!">Link 2</a></li>
-                <li><a class="white-text" href="#!">Link 3</a></li>
-                <li><a class="white-text" href="#!">Link 4</a></li>
-              </ul>
-            </div>
-            <div class="col l3 s12">
-              <h5 class="white-text">Connect</h5>
-              <ul>
-                <li><a class="white-text" href="#!">Link 1</a></li>
-                <li><a class="white-text" href="#!">Link 2</a></li>
-                <li><a class="white-text" href="#!">Link 3</a></li>
-                <li><a class="white-text" href="#!">Link 4</a></li>
-              </ul>
-            </div>
+      <footer class="page-footer green lighten-4">
+      <div class="container">
+        <div class="row">
+          <div class="col s12 m6">
+            <h5 class="green-text text-darken-3">Clube da Caçamba</h5>
+            <ul>
+              <li><a href="#!" class="green-text text-darken-3">Início</a></li>
+              <li><a href="#!" class="green-text text-darken-3">Sobre</a></li>
+              <li><a href="#!" class="green-text text-darken-3">Projetos</a></li>
+              <li><a href="#!" class="green-text text-darken-3">Blog</a></li>
+              <li><a href="#!" class="green-text text-darken-3">Parceiros</a></li>
+              <li><a href="#!" class="green-text text-darken-3">Contato</a></li>
+            </ul>
+          </div>
+          <div class="col s12 m6 right-align">
+            <h6 class="green-text text-darken-3">Nos encontre nas redes sociais</h6>
+            <a href="#!" class="green-text text-darken-3"><i class="material-icons">facebook</i></a>
+            <a href="#!" class="green-text text-darken-3"><i class="material-icons">instagram</i></a>
           </div>
         </div>
-        <div class="footer-copyright">
-          <div class="container">
-            Made by <a class="brown-text text-lighten-3" href="http://materializecss.com">Materialize</a>
-          </div>
-        </div>
-      </footer>
+      </div>
+    </footer>
     `;
   }
   
--- js/components/pagehome.js
+++ js/components/pagehome.js
@@ -48,11 +48,120 @@ export default function home() {
     <img src="images/next.svg" class="carousel-next" style="position: absolute; top: 50%; right: 10px; cursor: pointer; width: 40px; height: 40px; transform: translateY(-50%);">
     </div>
 
-    <div class="container">
-      <h3>Conheça mais sobre nós...</h3>
-      <p>Somos membros de um projeto vinculado à Universidade Estadual de Londrina (UEL) promovemos conhecimento científico sobre desenvolvimento sustentável.</p>
-      <a href=""> Ver mais</a>
+    <div class="container row">
+      <div class="col s6">
+        <h3>Conheça mais sobre nós...</h3>
+        <p>Somos membros de um projeto vinculado à Universidade Estadual de Londrina (UEL) promovemos conhecimento científico sobre desenvolvimento sustentável.</p>
+        <a href=""> Ver mais</a>
       </div>
+      <div class="col s6">
+        <img src="images/sobre.png?text=Slide+2" alt="Slide 2">
+      </div>
+    </div>
+
+    <section class="section">
+      <div class="container">
+        <h4 class="white-text center-align">Nossos conteúdos</h4>
+        <div class="row">
+          <!-- Conteúdo 1 -->
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-image">
+                <img src="caminho-da-imagem-1.jpg" alt="Comitê de Sustentabilidade">
+              </div>
+              <div class="card-content">
+                <span class="card-title">Comitê de Sustentabilidade</span>
+                <p>Parceria entre CPAC, Clube da Caçamba, Desplastifica UEL e Ionarte para promover um evento sustentável!</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Conteúdo 2 -->
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-image">
+                <img src="caminho-da-imagem-2.jpg" alt="Comitê de Sustentabilidade">
+              </div>
+              <div class="card-content">
+                <span class="card-title">Comitê de Sustentabilidade</span>
+                <p>Parceria entre CPAC, Clube da Caçamba, Desplastifica UEL e Ionarte para promover um evento sustentável!</p>
+              </div>
+            </div>
+          </div>
+
+          <!-- Conteúdo 3 -->
+          <div class="col s12 m4">
+            <div class="card">
+              <div class="card-image">
+                <img src="caminho-da-imagem-3.jpg" alt="Comitê de Sustentabilidade">
+              </div>
+              <div class="card-content">
+                <span class="card-title">Comitê de Sustentabilidade</span>
+                <p>Parceria entre CPAC, Clube da Caçamba, Desplastifica UEL e Ionarte para promover um evento sustentável!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="center-align">
+          <a href="link-para-mais-conteudos.html" class="white-text">ver mais conteúdos</a>
+        </div>
+      </div>
+    </section>
+
+    <!-- Seção: Saiba quem são os nossos parceiros -->
+      <section class="section white">
+        <div class="container">
+          <h4 class="center-align text-darken-3">Saiba quem são os nossos parceiros</h4>
+          <div class="row center-align">
+            <div class="col s12 m2">
+              <div class="card-panel parceiros">
+                <img src="images/desplastifica.jpeg" alt="Imagem 2" class="responsive-img">
+              </div>
+            </div>
+            <div class="col s12 m2">
+              <div class="card-panel parceiros">
+                <img src="images/leca.jpeg" alt="Imagem 3" class="responsive-img">
+              </div>
+            </div>
+            <div class="col s12 m2">
+              <div class="card-panel parceiros">
+                <img src="images/lunarte.jpeg" alt="Imagem 4" class="responsive-img">
+              </div>
+            </div>
+            <div class="col s12 m2">
+              <div class="card-panel parceiros">
+                <img src="images/ninter.jpeg" alt="Imagem 5" class="responsive-img">
+              </div>
+            </div>
+            <div class="col s12 m2">
+              <div class="card-panel parceiros">
+                <img src="images/observatorio.jpeg" alt="Imagem 5" class="responsive-img">
+              </div>
+            </div>
+          </div>
+          <p class="center-align">Somos membros de um projeto vinculado à Universidade Estadual de Londrina (UEL) promovemos conhecimento científico sobre desenvolvimento sustentável.</p>
+          <div class="center-align">
+            <a href="#!" class="text-darken-3">ver mais</a>
+          </div>
+        </div>
+      </section>
+      <!-- Seção: Entre em contato com a gente -->
+      <section class="section white">
+        <div class="container">
+          <h4 class="center-align text-darken-3">Entre em contato com a gente!</h4>
+          <div class="row">
+            <div class="col s12 m6">
+              <p>Somos membros de um projeto vinculado à Universidade Estadual de Londrina (UEL) promovemos conhecimento científico sobre desenvolvimento sustentável.</p>
+              <div class="center-align">
+                <a href="#!" class="text-darken-3">ver mais</a>
+              </div>
+            </div>
+            <div class="col s12 m6 center-align">
+              <img src="/images/contato.png" alt="Ícone de contato" class="responsive-img" style="max-width: 150px;">
+            </div>
+          </div>
+        </div>
+      </section>
   `;
 
   reinicializarMaterialize();
