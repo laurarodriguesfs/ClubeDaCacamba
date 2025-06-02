c0c997a8dad611ade6787285b40836bdc5c7c075
laurarodriguesfs <contato@laurarodrigues.com.br>
2025-06-02 15:40:13 -0300

    Ajustes de posicionamento e estrututa

--- js/components/pagesobre.js
+++ js/components/pagesobre.js
@@ -2,10 +2,7 @@ export default function sobre (){
 const main = document.querySelector("#main")
 
 main.innerHTML=`
-   <div id="main" class="container">
-    <div class="section">
-      <p>CONTEUDO DO SOBRE</p>
-    </div>
+   <div id="main" class="page-sobre container">
       <h1 class="center-align">Membros</h1>
       <div class="row">
         <div class="col s6 l2 offset-l2 membro-div">
@@ -23,32 +20,32 @@ main.innerHTML=`
           <p>Nome e sobrenome</p>
           <p>Cargo dentro da ong</p>
         </div>
-        <div class="col s6 l2 membro-div">
+        <div class="col s6 l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/karsten-winegeart-nCk-Pb0Lll8-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
           <p>Cargo dentro da ong</p>
         </div>
-        <div class="col s6 l2 offset-l2 membro-div">
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/karsten-winegeart-W9c6YicHKyE-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
           <p>Cargo dentro da ong</p>
         </div>
-        <div class="col s6 l2 offset-l2 membro-div">
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/muneeb-syed-ruaRIIP9VH8-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
           <p>Cargo dentro da ong</p>
         </div>
-        <div class="col s6 l2 offset-l2 membro-div">
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-pOUA8Xay514-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
           <p>Cargo dentro da ong</p>
         </div>
-        <div class="col s6 l2 offset-l2 membro-div">
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/matthew-henry-2Ts5HnA67k8-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
           <p>Cargo dentro da ong</p>
         </div>
-        <div class="col s12 l2 offset-l2 membro-div">
+        <div class="col s12 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/matthew-henry-U5rMrSI7Pn4-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
           <p>Cargo dentro da ong</p>
