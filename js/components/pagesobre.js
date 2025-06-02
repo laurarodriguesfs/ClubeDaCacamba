9eadb8f5a045bdd79df4c2f8b712e7864f9a4ac5
Poc c <pocc727@gmail.com>
2025-06-02 00:35:21 -0300

    adição da área de membros

--- js/components/pagesobre.js
+++ js/components/pagesobre.js
@@ -6,6 +6,54 @@ main.innerHTML=`
     <div class="section">
       <p>CONTEUDO DO SOBRE</p>
     </div>
+      <h1 class="center-align">Membros</h1>
+      <div class="row">
+        <div class="col s6 l2 offset-l2 membro-div">
+          <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo dentro da ong</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div">
+          <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-DziZIYOGAHc-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo dentro da ong</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div">
+          <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-lJJlaUWYrPE-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo dentro da ong</p>
+        </div>
+        <div class="col s6 l2 membro-div">
+          <div><img class="foto-membros" src="images/imagens de cachorro/karsten-winegeart-nCk-Pb0Lll8-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo dentro da ong</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div">
+          <div><img class="foto-membros" src="images/imagens de cachorro/karsten-winegeart-W9c6YicHKyE-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo dentro da ong</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div">
+          <div><img class="foto-membros" src="images/imagens de cachorro/muneeb-syed-ruaRIIP9VH8-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo dentro da ong</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div">
+          <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-pOUA8Xay514-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo dentro da ong</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div">
+          <div><img class="foto-membros" src="images/imagens de cachorro/matthew-henry-2Ts5HnA67k8-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo dentro da ong</p>
+        </div>
+        <div class="col s12 l2 offset-l2 membro-div">
+          <div><img class="foto-membros" src="images/imagens de cachorro/matthew-henry-U5rMrSI7Pn4-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo dentro da ong</p>
+        </div>
+      </div>
   </div>
 `;
 }
