69205f0238ed7fc6a4f35e091507c81d08b0755b
poc <pocc727@gmail.com>
2025-06-02 23:29:10 -0300

    Adição de História e Objetivos na página Sobre

--- js/components/pagesobre.js
+++ js/components/pagesobre.js
@@ -3,54 +3,122 @@ const main = document.querySelector("#main")
 
 main.innerHTML=`
    <div id="main" class="page-sobre container">
+      <h1 class="center-align">História</h1>
+      <p>O Clube da Caçamba foi criado e idealizado pela Professora Doutora Verônica Bender Haydu e desenvolvido pelos estagiários do curso de Psicologia da Universidade Estadual de Londrina (UEL), estudantes de iniciação científica e alunos do doutorado em Análise do Comportamento. O objetivo do projeto é divulgar conhecimentos científicos sobre temáticas socioambientais e nosso referencial teórico é a Análise do Comportamento. O nome “Clube da Caçamba” deriva de nosso interesse em sensibilizar nosso público sobre o adequado tratamento e destinação de resíduos sólidos. Atualmente, além de produzirmos postagens em nossas páginas do Instagram e do Facebook, também realizamos publicações científicas em revistas e eventos e trabalhamos com atividades interativas para sensibilização ambiental do público universitário.</p>
+
+      <h1 class="center-align">Objetivos</h1>
+      <ul class="collapsible popout">
+        <li>
+          <div class="collapsible-header">Divulgação científica<i class="material-icons">add_circle</i></div>
+          <div class="collapsible-body"><span>Realizar a divulgação científica de diferentes temas da área do desenvolvimento sustentável, com foco na destinação adequada de resíduos.</span></div>
+        </li>
+        <li>
+          <div class="collapsible-header">Rede colaborativa<i class="material-icons">add_circle</i></div>
+          <div class="collapsible-body"><span>Estabelecer uma metacontingência para a promoção de contingências entrelaçadas entre professores, estudantes e seguidores do projeto que produzam um produto agregado e consequências culturais. Ou seja, criar relações que se mantenham e que sejam passadas entre gerações.</span></div>
+        </li>
+        <li>
+          <div class="collapsible-header">Modelo replicável<i class="material-icons">add_circle</i></div>
+          <div class="collapsible-body"><span>Fornecer modelo para que projetos semelhantes sejam criados em outras instituições de ensino.</span></div>
+        </li>
+      </ul>
+
       <h1 class="center-align">Membros</h1>
       <div class="row">
         <div class="col s6 l2 offset-l2 membro-div">
           <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-Mv9hjnEUHR4-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
-          <p>Cargo dentro da ong</p>
+          <p>Cargo</p>
         </div>
         <div class="col s6 l2 offset-l2 membro-div">
           <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-DziZIYOGAHc-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
-          <p>Cargo dentro da ong</p>
+          <p>Cargo</p>
         </div>
         <div class="col s6 l2 offset-l2 membro-div">
           <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-lJJlaUWYrPE-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
-          <p>Cargo dentro da ong</p>
+          <p>Cargo</p>
         </div>
         <div class="col s6 l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/karsten-winegeart-nCk-Pb0Lll8-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
-          <p>Cargo dentro da ong</p>
+          <p>Cargo</p>
         </div>
         <div class="col s6 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/karsten-winegeart-W9c6YicHKyE-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
-          <p>Cargo dentro da ong</p>
+          <p>Cargo</p>
         </div>
         <div class="col s6 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/muneeb-syed-ruaRIIP9VH8-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
-          <p>Cargo dentro da ong</p>
+          <p>Cargo</p>
         </div>
         <div class="col s6 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-pOUA8Xay514-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
-          <p>Cargo dentro da ong</p>
+          <p>Cargo</p>
         </div>
         <div class="col s6 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/matthew-henry-2Ts5HnA67k8-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
-          <p>Cargo dentro da ong</p>
+          <p>Cargo</p>
         </div>
-        <div class="col s12 l2 offset-l2 membro-div margem-membro">
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
           <div><img class="foto-membros" src="images/imagens de cachorro/matthew-henry-U5rMrSI7Pn4-unsplash.jpg" alt="foto de perfil"></div>
           <p>Nome e sobrenome</p>
-          <p>Cargo dentro da ong</p>
+          <p>Cargo</p>
+        </div>
+        <div class="col s6 l2 membro-div margem-membro">
+          <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-K4mSJ7kc0As-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
+          <div><img class="foto-membros" src="images/imagens de cachorro/karsten-winegeart-NE0XGVKTmcA-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
+          <div><img class="foto-membros" src="images/imagens de cachorro/karsten-winegeart-WaMMo0it6Vg-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
+          <div><img class="foto-membros" src="images/imagens de cachorro/matthew-henry-6x-hVXXiBxs-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
+          <div><img class="foto-membros" src="images/imagens de cachorro/flouffy-7hEXd9kYPCY-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
+          <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-AQRp2NH-O8k-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo</p>
+        </div>
+        <div class="col s6 l2 membro-div margem-membro">
+          <div><img class="foto-membros" src="images/imagens de cachorro/toshi-K5pLGYJMHKk-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
+          <div><img class="foto-membros" src="images/imagens de cachorro/charlesdeluvio-bYXP-ITv4_s-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo</p>
+        </div>
+        <div class="col s6 l2 offset-l2 membro-div margem-membro">
+          <div><img class="foto-membros" src="images/imagens de cachorro/karsten-winegeart-BVqQNu5J7qI-unsplash.jpg" alt="foto de perfil"></div>
+          <p>Nome e sobrenome</p>
+          <p>Cargo</p>
         </div>
       </div>
   </div>
 `;
+
+  const elems = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(elems);
+
 }
