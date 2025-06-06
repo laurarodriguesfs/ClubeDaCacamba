965ccea14f682d8e9d5f541e08d21541eee56c73
Pedro7776 <sobrinhop605@gmail.com>
2025-06-06 15:32:52 -0300

    Adiciona html a função de parceiros

--- js/components/pageparceiros.js
+++ js/components/pageparceiros.js
@@ -5,8 +5,34 @@ const main = document.querySelector("#main")
 main.innerHTML=`
  <div class="container">
     <div class="section">
-      <p>CONTEUDO DA PAGINA DE Parceiros</p>
-    </div>
-  </div>
+      <h1>Uma rede de parceiros</h1>
+      
+        <div class="logos">
+          <img src="observatorio.JPG" alt="Observatório de Resíduos">
+          <img src="ninter.JPG" alt="Ninter UEL">
+          <img src="lonarte.JPG" alt="Lonarte">
+          <img src="leca.JPG" alt="LECA">
+          <img src="desplastifica.JPG" alt="Desplastifica UEL">
+        </div>
+      
+        <div class="subtitulo">
+          Venha começar<br />
+          uma parceria com a gente!
+        </div>
+      
+        <p class="paragrafo">
+          Somos membros de um projeto vinculado<br />
+          à Universidade Estadual de Londrina (UEL)<br />
+          promovemos conhecimento científico<br />
+          sobre desenvolvimento sustentável.
+        </p>
+      
+        <a href="#parceiros" class="vermais">Ver mais</a>
+      
+        <!-- Âncora do link -->
+        <div id="parceiros" style="margin-top: 100px;">
+        </div>
+       </div>
+      </div>
 `;
 }
