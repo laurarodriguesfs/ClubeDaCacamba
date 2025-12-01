a8be7b44a521a10bad3211fc987804b800ba61be
Poc-c-727 <pocc727@gmail.com>
2025-12-01 13:31:47 -0300

    Adição de visibilidade de senha no login

--- js/components/admin/pages/login.js
+++ js/components/admin/pages/login.js
@@ -9,7 +9,15 @@ function renderLoginPage() {
                     <span class="card-title">Acesso Administrativo</span>
                     <form id="login-form">
                     <div class="input-field"><input id="email" type="email" class="validate" required><label for="email">Email</label></div>
-                    <div class="input-field"><input id="password" type="password" class="validate" required><label for="password">Senha</label></div>
+
+                    <div class="input-field password-wrapper" style="position: relative;">
+                        <input id="password" type="password" class="validate" required>
+                        <label for="password">Senha</label>
+                        <span class="suffix-icon" style="position: absolute; right: 10px; top: 65%; transform: translateY(-50%); cursor: pointer;">
+                            <i class="material-icons visibility-icon">visibility_off</i>
+                        </span>
+                    </div>
+
                     <button class="btn waves-effect waves-light" type="submit">Entrar<i class="material-icons right">send</i></button>
                     <p id="error-message" class="red-text"></p>
                     </form>
@@ -17,5 +25,24 @@ function renderLoginPage() {
             </div></div>
         </div>`;
     $mainContainer.html(loginHTML);
+
+    $('.visibility-icon').click(function(){
+        const passwordInput = $('#password');
+        const icon = $(this);
+        
+        if (passwordInput.attr('type') === 'password') 
+        {
+            passwordInput.attr('type', 'text');
+            icon.text('visibility');
+        } else 
+            {
+            passwordInput.attr('type', 'password');
+            icon.text('visibility_off');
+        }
+        
+        // Mantém o foco no input
+        passwordInput.focus();
+    });
+
     $('#login-form').on('submit', handleLogin);
 }
